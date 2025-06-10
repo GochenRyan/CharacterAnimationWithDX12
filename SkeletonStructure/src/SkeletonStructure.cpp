71a0eec3c3ace88c929db5f30f6bfcf02a16b8fe
Gaocheng <rangaocheng@outlook.com>
2025-06-10 19:01:36 +0800

    Add FBX resources

--- SkeletonStructure/src/SkeletonStructure.cpp
+++ SkeletonStructure/src/SkeletonStructure.cpp
@@ -1,6 +1,10 @@
 #include <fbxsdk.h>
+#include <string>
+#include <iostream>
 
 int main()
 {
+    std::string fbx_path = std::string(FBX_RESOURCES_DIR) + "/vietnam-helmet-free/source/Helmet1.fbx";
+    std::cout << "FBX Path: " << fbx_path << std::endl;
     return 0;
 }
