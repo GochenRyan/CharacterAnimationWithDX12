663d4e1c6a2777d8b54034fca812fd3d43fe88ba
Gaocheng <rangaocheng@outlook.com>
2025-06-12 18:47:16 +0800

    Print bone hierarchy

--- SkeletonStructure/src/SkeletonStructure.cpp
+++ SkeletonStructure/src/SkeletonStructure.cpp
@@ -2,89 +2,32 @@
 #include <string>
 #include <iostream>
 
-/* Tab character ("\t") counter */
-int numTabs = 0;
+static void PrintBoneHierarchy(FbxNode* pNode, int depth)
+{
+    if (pNode == nullptr)
+        return;
 
-/**
- * Print the required number of tabs.
- */
-void PrintTabs() {
-    for (int i = 0; i < numTabs; i++)
-        printf("\t");
-}
+    auto attribute = pNode->GetNodeAttribute();
 
-/**
- * Return a string-based representation based on the attribute type.
- */
-FbxString GetAttributeTypeName(FbxNodeAttribute::EType type) {
-    switch (type) {
-    case FbxNodeAttribute::eUnknown: return "unidentified";
-    case FbxNodeAttribute::eNull: return "null";
-    case FbxNodeAttribute::eMarker: return "marker";
-    case FbxNodeAttribute::eSkeleton: return "skeleton";
-    case FbxNodeAttribute::eMesh: return "mesh";
-    case FbxNodeAttribute::eNurbs: return "nurbs";
-    case FbxNodeAttribute::ePatch: return "patch";
-    case FbxNodeAttribute::eCamera: return "camera";
-    case FbxNodeAttribute::eCameraStereo: return "stereo";
-    case FbxNodeAttribute::eCameraSwitcher: return "camera switcher";
-    case FbxNodeAttribute::eLight: return "light";
-    case FbxNodeAttribute::eOpticalReference: return "optical reference";
-    case FbxNodeAttribute::eOpticalMarker: return "marker";
-    case FbxNodeAttribute::eNurbsCurve: return "nurbs curve";
-    case FbxNodeAttribute::eTrimNurbsSurface: return "trim nurbs surface";
-    case FbxNodeAttribute::eBoundary: return "boundary";
-    case FbxNodeAttribute::eNurbsSurface: return "nurbs surface";
-    case FbxNodeAttribute::eShape: return "shape";
-    case FbxNodeAttribute::eLODGroup: return "lodgroup";
-    case FbxNodeAttribute::eSubDiv: return "subdiv";
-    default: return "unknown";
+    if (attribute->GetAttributeType() == FbxNodeAttribute::EType::eSkeleton)
+    {
+        auto pSkeleton = pNode->GetSkeleton();
+        for (int i = 0; i < depth; ++i)
+            std::cout << "  ";
+        std::cout << pNode->GetName();
+
+        auto pParent = pNode->GetParent();
+        if (pParent)
+        {
+            std::cout << " (Parent: " << pParent->GetName() << ")";
+        }
+        std::cout << std::endl;
+
+        for (int i = 0; i < pNode->GetChildCount(); ++i)
+        {
+            PrintBoneHierarchy(pNode->GetChild(i), depth + 1);
+        }
     }
-}
-
-/**
- * Print an attribute.
- */
-void PrintAttribute(FbxNodeAttribute* pAttribute) {
-    if (!pAttribute) return;
-
-    FbxString typeName = GetAttributeTypeName(pAttribute->GetAttributeType());
-    FbxString attrName = pAttribute->GetName();
-    PrintTabs();
-    // Note: to retrieve the character array of a FbxString, use its Buffer() method.
-    printf("<attribute type='%s' name='%s'/>\n", typeName.Buffer(), attrName.Buffer());
-}
-
-/**
-* Print a node, its attributes, and all its children recursively.
-*/
-void PrintNode(FbxNode* pNode) {
-    PrintTabs();
-    const char* nodeName = pNode->GetName();
-    FbxDouble3 translation = pNode->LclTranslation.Get();
-    FbxDouble3 rotation = pNode->LclRotation.Get();
-    FbxDouble3 scaling = pNode->LclScaling.Get();
-
-    // Print the contents of the node.
-    printf("<node name='%s' translation='(%f, %f, %f)' rotation='(%f, %f, %f)' scaling='(%f, %f, %f)'>\n",
-        nodeName,
-        translation[0], translation[1], translation[2],
-        rotation[0], rotation[1], rotation[2],
-        scaling[0], scaling[1], scaling[2]
-    );
-    numTabs++;
-
-    // Print the node's attributes.
-    for (int i = 0; i < pNode->GetNodeAttributeCount(); i++)
-        PrintAttribute(pNode->GetNodeAttributeByIndex(i));
-
-    // Recursively print the children.
-    for (int j = 0; j < pNode->GetChildCount(); j++)
-        PrintNode(pNode->GetChild(j));
-
-    numTabs--;
-    PrintTabs();
-    printf("</node>\n");
 }
 
 int main()
@@ -131,7 +74,7 @@ int main()
     FbxNode* lRootNode = lScene->GetRootNode();
     if (lRootNode) {
         for (int i = 0; i < lRootNode->GetChildCount(); i++)
-            PrintNode(lRootNode->GetChild(i));
+            PrintBoneHierarchy(lRootNode->GetChild(i), 0);
     }
     // Destroy the SDK manager and all the other objects it was handling.
     lSdkManager->Destroy();
